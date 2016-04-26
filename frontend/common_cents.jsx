e618bdba22ab22d1e47b8316fbe13a0e3aa5fdc9
Alastair Cleve <AlastairCleve@Alastairs-MacBook-Pro.local>
2016-04-26 13:59:28 -0700

    Set up common_cents.jsx and root

---
+++ frontend/common_cents.jsx
@@ -0,0 +1,33 @@
+//React
+var React = require('react');
+var ReactDOM = require('react-dom');
+//Router
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
+
+var App = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <header><h1>Common &#xa2;ents</h1></header>
+      </div>
+    );
+  }
+});
+
+//Note: LoginForm goes below this.props.children
+
+var Router = (
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>
+);
+
+document.addEventListener('DOMContentLoaded', function(){
+  var root = document.getElementById('content');
+  ReactDOM.render(Router, root);
+});
