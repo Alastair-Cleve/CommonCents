8e201c92aa1d56d1401d7dcf55a8be9e47e90a38
Alastair Cleve <AlastairCleve@Alastairs-MacBook-Pro.local>
2016-05-02 14:26:47 -0700

    Created seed file

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bcrypt'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+require('faker')
+
+currencies = ["EUR", "AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK",
+"DKK", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "JPY", "KRW",
+"MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD",
+"THB", "TRY", "USD", "ZAR"]
+
 User.create!(username: "guest", password: "password", default_currency: "USD")
+
+24.times do
+  User.create!(username: Faker::Internet.user_name,
+               password: Faker::Internet.password,
+               default_currency: currencies.sample)
+end
+
+5.times do
+  Transfer.create!(transferor_id: 1,
+                   transferee_id: (1..25).to_a.sample),
+                   amount: rand(1000..5000) + rand(),
+                   currency: currencies.sample)
+end
